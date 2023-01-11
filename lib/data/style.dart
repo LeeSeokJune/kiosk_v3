71af8adcca34847fa9fefbe49bd1b0f67c89619f
LeeSeokJune <indians1720@gmail.com>
2023-01-11 23:00:25 +0900

    color

--- lib/data/style.dart
+++ lib/data/style.dart
@@ -2,3 +2,6 @@ import 'package:flutter/material.dart';
 
 final test_line = BoxDecoration(border: Border.all(color: main_color));
 final main_color = Color.fromRGBO(13, 64, 151, 1);
+final mint_color = Color.fromRGBO(137, 241, 207, 1);
+final background_color = Color.fromRGBO(228, 239, 234, 1);
+final grey_color = Color.fromRGBO(229, 229, 229, 1);
